2a8219c178da681670634e7b975ea0d04de128c3
Paulo Barros <clementedbarros@gmail.com>
2020-08-25 22:49:52 -0300

    test: add missing test

--- core/src/modules/appointments/services/CreateAppointmentService.ts
+++ core/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,4 @@
-import { startOfHour } from 'date-fns';
+import { startOfHour, isBefore, getHours } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 import AppError from '@shared/errors/AppError';
 import Appointment from '../infra/typeorm/entities/Appointment';
@@ -22,7 +22,15 @@ class CreateAppointmentService {
 		date,
 	}: IRequestDTO): Promise<Appointment> {
 		const appointmentDate = startOfHour(date);
-
+		if (isBefore(appointmentDate, Date.now())) {
+			throw new AppError("You can't create an appointment on past date");
+		}
+		if (user_id === provider_id) {
+			throw new AppError("You can't create an appointment for yourself");
+		}
+		if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
+			throw new AppError('You can only create appointment between 8am and 5pm');
+		}
 		const findInSameDate = await this.appointmentsRepository.findByDate(
 			appointmentDate,
 		);
