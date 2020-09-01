c07e675d0b6fb7fe64a76875afa791077cc896f1
Paulo Barros <clementedbarros@gmail.com>
2020-08-31 21:47:08 -0300

    fix: add validation in routes

--- core/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ core/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '../controllers/AppointmentsController';
 import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
@@ -7,12 +8,17 @@ const appointmentsRouter = Router();
 appointmentsRouter.use(ensureAuthenticated);
 const appointmentsController = new AppointmentsController();
 const providerAppointmentsController = new ProviderAppointmentsController();
-/* appointmentsRouter.get('/', async (request, response) => {
-	const appointmentsRepository = getCustomRepository(AppointmentsRepository);
-	const appointment = await appointmentsRepository.find();
-	return response.json(appointment);
-}); */
-appointmentsRouter.post('/', appointmentsController.create);
+
+appointmentsRouter.post(
+	'/',
+	celebrate({
+		[Segments.BODY]: {
+			provider_id: Joi.string().uuid().required(),
+			date: Joi.date(),
+		},
+	}),
+	appointmentsController.create,
+);
 appointmentsRouter.get('/me', providerAppointmentsController.index);
 
 export default appointmentsRouter;
--- core/src/modules/users/infra/http/routes/profile.routes.ts
+++ core/src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
+
 import ProfileController from '../controllers/ProfileController';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
@@ -6,7 +8,19 @@ const profileRouter = Router();
 const profileController = new ProfileController();
 
 profileRouter.use(ensureAuthenticated);
-profileRouter.put('/', profileController.update);
 profileRouter.get('/', profileController.show);
+profileRouter.put(
+	'/',
+	celebrate({
+		[Segments.BODY]: {
+			name: Joi.string().required(),
+			email: Joi.string().email().required(),
+			password: Joi.string(),
+			old_password: Joi.string(),
+			password_confirmation: Joi.string().valid(Joi.ref('password')),
+		},
+	}),
+	profileController.update,
+);
 
 export default profileRouter;
