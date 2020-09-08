503367fc5a1af697c244644b6466b361a2828b4e
Paulo Barros <clementedbarros@gmail.com>
2020-09-08 16:31:14 -0300

    feat: add request limiter

--- core/src/shared/infra/http/server.ts
+++ core/src/shared/infra/http/server.ts
@@ -6,13 +6,14 @@ import { errors } from 'celebrate';
 import cors from 'cors';
 import AppError from '@shared/errors/AppError';
 import uploadConfig from '@config/upload';
+import rateLimiter from './middlewares/rateLimiter';
 
 import routes from './routes';
 import '@shared/infra/typeorm';
 import '@shared/container';
 
 const app = express();
-
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
