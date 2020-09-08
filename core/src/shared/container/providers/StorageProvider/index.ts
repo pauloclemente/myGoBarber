0e580ba8dda5b8587d45b3be81810a026eb421cc
Paulo Barros <clementedbarros@gmail.com>
2020-09-08 10:32:29 -0300

    feat: add upload avatar img to AWS s3

--- core/src/shared/container/providers/StorageProvider/index.ts
+++ core/src/shared/container/providers/StorageProvider/index.ts
@@ -2,11 +2,10 @@ import { container } from 'tsyringe';
 
 import IStorageProvider from './models/IStorageProvider';
 import DiskStorageProvider from './implementations/DisKStorageProvider';
+import S3StorageProvider from './implementations/S3StorageProvider';
 
 const providers = {
 	disk: DiskStorageProvider,
+	s3: S3StorageProvider,
 };
-container.registerSingleton<IStorageProvider>(
-	'StorageProvider',
-	providers.disk,
-);
+container.registerSingleton<IStorageProvider>('StorageProvider', providers.s3);
