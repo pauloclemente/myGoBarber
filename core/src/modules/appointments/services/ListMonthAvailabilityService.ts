38c421b3cd5af5b69b3c2e347e60b606275d4764
Paulo Barros <clementedbarros@gmail.com>
2020-09-10 22:17:34 -0300

    fix: put false in to day before current day when list month availability

--- core/src/modules/appointments/services/ListMonthAvailabilityService.ts
+++ core/src/modules/appointments/services/ListMonthAvailabilityService.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 import { injectable, inject } from 'tsyringe';
-import { getDaysInMonth, getDate } from 'date-fns';
+import { getDaysInMonth, getDate, isAfter } from 'date-fns';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
 interface IRequest {
@@ -40,12 +40,14 @@ export default class ListMonthAvailabilityService {
 			(_, index) => index + 1,
 		);
 		const availability = eachDayArray.map((day) => {
+			const compareDate = new Date(year, month - 1, day, 23, 59, 59);
 			const appointmentsInDay = appointments.filter((appointment) => {
 				return getDate(appointment.date) === day;
 			});
 			return {
 				day,
-				available: appointmentsInDay.length < 10,
+				available:
+					isAfter(compareDate, new Date()) && appointmentsInDay.length < 10,
 			};
 		});
 		return availability;
